7c147e4bbae78977f87e66aaf6fdc207fe10be89
Vika <vigrinshpum@gmail.com>
2022-08-21 16:54:19 +0300

    DZ1

---
+++ lesson1/dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a>b)
+{
+    Console.Write ("max = ");
+    Console.WriteLine (a);
+    Console.Write ("min = ");
+    Console.WriteLine (b);
+}
+else
+{
+    Console.Write ("max = ");
+    Console.WriteLine (b);
+    Console.Write ("min = ");
+    Console.WriteLine (a);
+}
