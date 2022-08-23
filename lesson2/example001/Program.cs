4504b00a4a8388c3b9e19114a08beb1ab674465a
Vika <vigrinshpum@gmail.com>
2022-08-23 17:35:53 +0300

    expirion2

--- lesson2/example001/Program.cs
+++ lesson2/example001/Program.cs
@@ -13,10 +13,14 @@ int a2 = 12;
 int b2 = 23;
 int c2 = 332;
 int a3 = 13;
-int b3 = 23;
+int b3 = 1123;
 int c3 = 33;
-int max1 = Max(a1, b1, c1);
+/* int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+int max3 = Max(a3, b3, c3); */
+//int max = Max(max1, max2, max3);
+int max = Max(
+ Max(a1, b1, c1),
+ Max(a2, b2, c2), 
+ Max(a3, b3, c3));
 Console.WriteLine(max);
