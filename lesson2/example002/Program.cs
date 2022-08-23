b14e4d987b75b4c431dc4ec37119920c9fcdf04d
Vika <vigrinshpum@gmail.com>
2022-08-23 18:44:02 +0300

    example003

---
+++ lesson2/example002/Program.cs
@@ -0,0 +1,14 @@
+﻿int []array = {4, 35, 3, 5, 15, 5};
+int n = array.Length;
+int find = 35;
+int index = 0;
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
