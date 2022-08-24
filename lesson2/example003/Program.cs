a07a670fa749bc33deda2950f8f73685b27cadaf
Vika <vigrinshpum@gmail.com>
2022-08-24 11:11:58 +0300

    void

---
+++ lesson2/example003/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray (int [] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 5);
+        index++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+ }
+
+ int []array = new int [5];
+ FillArray (array);
+ PrintArray (array);
