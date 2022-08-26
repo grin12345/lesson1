66bc6f718e955127654c01f694eb802dfef3c92c
Vika <vigrinshpum@gmail.com>
2022-08-26 22:59:30 +0300

    example008

---
+++ lesson2/seminar/example008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a*a==b) Console.WriteLine ("да");
+else if (b*b==a) Console.WriteLine ("да");
+else Console.WriteLine ("нет");
+    
