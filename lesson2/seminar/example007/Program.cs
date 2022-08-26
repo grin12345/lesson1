a2dd87da038074403b105a0e253ae0035dbf75c8
Vika <vigrinshpum@gmail.com>
2022-08-26 22:15:05 +0300

    example006

---
+++ lesson2/seminar/example007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a%23  == 0 && a%7  == 0)
+{
+    Console.WriteLine ("да");
+}
+else
+{
+    Console.WriteLine ("нет");
+}
+
+
