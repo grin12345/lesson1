22ff6c287918ffccc53db02594121ca2ee466c49
Vika <vigrinshpum@gmail.com>
2022-08-26 19:02:13 +0300

    example006

---
+++ lesson2/seminar/example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int res = a%b;
+if (res == 0)
+{
+    Console.WriteLine ("кратно");
+}
+else
+{
+     Console.WriteLine ("не кратно, остаток " +res);
+   }
