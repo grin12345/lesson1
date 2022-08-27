57625a7898b7193d9e21413ac7e59caa88380714
Vika <vigrinshpum@gmail.com>
2022-08-27 12:07:22 +0300

    DZ 2-1

---
+++ lesson2/DZ_2-1/Program.cs
@@ -0,0 +1,11 @@
+﻿// На вход принимает трехзначное число и на выходе показывае 2ую цифру этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+while (a < 100 || a > 999)
+{
+    Console.WriteLine("Введите трехзначное число: ");
+   a = int.Parse(Console.ReadLine());
+}
+int b = a / 10;
+int c = b % 10;
+Console.WriteLine(c);
